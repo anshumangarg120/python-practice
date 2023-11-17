c256a01ec20d218cab10bb6928feeaff73a490b6
Anshuman Garg <anshumangarg120@gmail.com>
2023-11-17 10:51:42 +0530

    test

---
+++ anish.py
@@ -0,0 +1,37 @@
+num1 = 10
+num2 = 2
+
+# Arithmetic Operators
+print("num1 + num2 is", num1 + num2)
+print("num1 - num2 is", num1 - num2)
+print("num1 * num2 is", num1 * num2)
+print("num1 / num2 is", num1 / num2)
+print("num1 // num2 is", num1 // num2)
+print("num1 ** num2 is", num1 ** num2)
+print("num1 % num2 is", num1 % num2)
+
+# Assigment Operators
+
+a = 4
+a += 2
+print(a)
+b = 4
+b -= 2
+print(b)
+
+# Comparision Operators
+x = 8
+y = 3
+z = 8
+print(x>y)
+print(x<y)
+print(x!=y) # 'X' 'Y' ke barbar nahi hai.
+print(x==z)
+
+# Logical Operators
+print(x==z and x>y)
+print(x==z and x<y)
+print(not(False)) # True
+print(not(True)) # False
+
+# Moudule & pip
---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello world")
+e = None
+print(e)
+
+f = "5"
+print(int(f)+1)
+
+g = 5
+print(g + 1)
